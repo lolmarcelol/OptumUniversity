7cb209c70f48dc355bba055a69c098ad13736624
KathLouise <kathe.nightwish@gmail.com>
2017-08-23 10:01:09 -0300

    mandando notas e periodo

---
+++ OptumUniversity/OptumUniversity/Models/Nota.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OptumUniversity.Models
+{
+    public enum Notas
+    {
+        A, B, C, D, E, F
+    }
+
+    public class Nota
+    {
+        public int NotaID { get; set; }
+        public int AlunoID { get; set; }
+        public int DisciplinaID { get; set; }
+        public Notas ? NotaAluno { get; set; }
+
+        public virtual Disciplina Disciplina{ get; set; }
+        public virtual Aluno Aluno { get; set; }
+    }
+}
---
+++ OptumUniversity/OptumUniversity/Models/Periodo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OptumUniversity.Models
+{
+    public enum Periodos
+    {
+        QuatroAnos = 8,
+        CincoAnos = 10
+    }
+
+    public class Periodo
+    {
+        public int PeriodoID { get; set; }
+        public int DisciplinaID { get; set; }
+        public int CursoID { get; set; }
+        public Periodos PeriodoCurso { get; set; }
+
+        public virtual Disciplina Disciplina { get; set; }
+        public virtual Curso Curso{ get; set; }
+
+    }
+}
